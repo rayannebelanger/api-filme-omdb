624dc01a2a9f21d40f041a3c5401a8f2e7e84788
Rayanne Belanger <135059362+rayannebelanger@users.noreply.github.com>
2024-05-05 18:26:26 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,23 @@
+function consultarFilmes() {
+    let filme = document.querySelector('#filme').value;
+    let apiKey = 'daaadaa7';
+    let url =  `http://www.omdbapi.com/?t=${filme}&apikey=${apiKey}`;
+
+    fetch(url).then(function(response){
+        response.json().then(mostrarFilme)
+    });
+
+    function mostrarFilme(dados) {
+        console.log(dados)
+        let resultado = document.querySelector('#resultado');
+        if(dados.erro) {
+            resultado.innerHTML = "Não foi possível localizar";
+        } else {
+            resultado.innerHTML = `<p>Filme: ${dados.Title}</p> 
+            <p>Ano: ${dados.Year}</p>
+            <p>Foto: </p>
+            <img src=${dados.Poster} alt="banner">`
+        }
+    }
+
+}
